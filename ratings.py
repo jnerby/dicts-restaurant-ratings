55402010c64ff9dcc74b89edfc40080c73cdecea
Jen Nerby <nerby.jennifer@gmail.com>
2021-11-08 16:59:56 -0700

    Add User Ratings

--- ratings.py
+++ ratings.py
@@ -2,12 +2,29 @@
 
 
 file = open("scores.txt")
+
+# initialize empty directory
 ratings = {}
 
+# asking user for new restaurant and rating
+new_restaurant = input("Restaurant name: ")
+new_score = input("Rating: ")
+
+# insert user entry into dictionary
+ratings[new_restaurant] = new_score
+
+# loop through scores.txt file to create dictionary
 for line in file:
+    # strip and split at colon
     line = line.strip().split(":")
+    # variable assignment
     restaurant = line[0]
     rating = line[1]
+    # insert into dictionary
     ratings[restaurant] = rating
 
-print(ratings)
+# sorting and printing for each key-value pair
+for restaurant, rating in sorted(ratings.items()):
+    print(f"{restaurant} is rated at {rating}.")
+
+file.close()
